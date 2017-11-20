12f3db0ca3f4086ff568ad42dfa659749f24b01e
Allan Lima <allancslima@gmail.com>
2017-11-20 03:03:16 -0200

    Improving database.php

--- inc/database.php
+++ inc/database.php
@@ -1,16 +1,26 @@
 <?php
-
+	
+	/**
+	*  Connects to the database
+	*/
 	function open_database() {
 		try {
+
 			$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 			mysqli_set_charset($connection, 'utf8');
 			return $connection;
+
 		} catch (Exception $e) {
+			
 			echo $e->getMessage();
 			return null;
+		
 		}
 	}
 
+	/**
+	*  Closes the database
+	*/
 	function close_database($connection) {
 		try {
 			mysqli_close($connection);
@@ -19,43 +29,57 @@
 		}
 	}
 
-	function find($table = null, $id = null, $is_fk = false, $fk_name = null) {
+	/**
+	*  Locates records with (also in child tables) or without the id parameter.
+	*/
+	function find($table, $id = null, $is_fk_id = false, $fk = null) {
 		$database = open_database();
 		$found = null;
 
 		try {
-			if ($id && !$is_fk && !$fk_name) {
-				$sql = "SELECT * FROM " . $table . " WHERE `id` = $id";
+
+			if ($id && !($is_fk_id || $fk)) {
+				$sql = "SELECT * FROM $table WHERE `id` = $id";
 				$result = $database->query($sql);
 				
 				if ($result->num_rows > 0)
 					$found = $result->fetch_assoc();
-			} else {
-				if ($is_fk && $fk_name)
-					$sql = "SELECT * FROM " . $table . " WHERE `$fk_name` = $id";
+			} 
+			else {
+				if ($id && $is_fk_id && $fk)
+					$sql = "SELECT * FROM $table WHERE `$fk` = $id";
 				else
-					$sql = 'SELECT * FROM ' . $table;
+					$sql = "SELECT * FROM $table";
 
 				$result = $database->query($sql);
+
 				if ($result->num_rows > 0)
 					$found = $result->fetch_all(MYSQLI_ASSOC);
 			}
+
 		}	catch (Exception $e) {
+
 			$_SESSION['message'] = $e->getMessage();
 			$_SESSION['type'] = 'danger';
+
 		}
 
 		close_database($database);
 		return $found;	
 	}
 
+	/**
+	*  Locates all records from a table
+	*/
 	function find_all($table) {
 		return find($table);
 	}
 
+	/**
+	*  Saves data in a table 
+	*/
 	function save($table = null, $data = null) {
 		$database = open_database();
-
 		$columns = null;
 		$values = null;
 
@@ -63,45 +87,59 @@
 			$columns .= "`" . trim($key, "'") . "`,";
 			$values .= "'$value',";
 		}
-		
 		$columns = rtrim($columns, ',');
 		$values = rtrim($values, ',');
-		$sql = "INSERT INTO " . $table . "($columns)" . " VALUES " . "($values)";
 
 		try {
+			
+			$sql = "INSERT INTO $table ($columns) VALUES ($values)";
 			$database->query($sql);
+			
 			$_SESSION['message'] = 'Registro cadastrado com sucesso';
 			$_SESSION['type'] = 'success';
+		
 		} catch (Exception $e) {
+
 			$_SESSION['message'] = 'Não foi possível realizar a operação.';
 			$_SESSION['type'] = 'danger';
+		
 		}
 
 		close_database($database);
 	}
 
+	/**
+	*  Updates data from a table record
+	*/
 	function update($table = null, $id = 0, $data = null) {
 		$database = open_database();
-
 		$items = null;
+
 		foreach ($data as $key => $value)
-			$items .= "`" . trim($key, "'") . "`" . "='$value',";
+			$items .= "`" . trim($key, "'") . "` = '$value',";
 		$items = rtrim($items, ',');
 
-		$sql = "UPDATE " . $table . " SET $items" . " WHERE `id` = $id";
-		
 		try {
+
+			$sql = "UPDATE $table SET $items WHERE `id` = $id";
 			$database->query($sql);
+
 			$_SESSION['message'] = 'Registro atualizado com sucesso';
 			$_SESSION['type'] = 'success';
+		
 		} catch (Exception $e) {
+
 			$_SESSION['message'] = 'Não foi possível realizar a operação.';
 			$_SESSION['type'] = 'danger';
+		
 		}
 
 		close_database($database);
 	}
 
+	/**
+	*  Removes records from parent and child tables
+	*/
 	function remove($table, $id, $is_parent = null, $child_table = null, $fk = null) {
 		$database = open_database();
 
@@ -112,6 +150,9 @@
 			
 			$sql = "DELETE FROM " . $table . " WHERE `id` = " . $id;
 			$database->query($sql);
+
+			$_SESSION['message'] = 'Registro removido com sucesso';
+			$_SESSION['type'] = 'success';
 		
 		} catch (Exception $e) {
 			
