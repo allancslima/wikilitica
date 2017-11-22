ebf6a7d4e5ecbfed98caa05aa7aed69f4d006501
Allan Lima <allancslima@gmail.com>
2017-11-22 07:50:15 -0200

    Showing state and city candidates in political party

--- political-parties/functions.php
+++ political-parties/functions.php
@@ -5,6 +5,8 @@
 
 	$political_parties = null;
 	$political_party = null;
+	$state_candidates = null;
+	$city_candidates = null;
 
 	function index() {
 		global $political_parties;
@@ -14,6 +16,16 @@
 	function view($id) {
 		global $political_party;
 		$political_party = find('political_parties', $id);
+	}
+
+	function state_candidates($political_party_id) {
+		global $state_candidates;
+		$state_candidates = find('state_candidates', $political_party_id, true, 'political_party_id');
+	}
+
+	function city_candidates($political_party_id) {
+		global $city_candidates;
+		$city_candidates = find('city_candidates', $political_party_id, true, 'political_party_id');
 	}
 
 	function add() {
--- political-parties/view.php
+++ political-parties/view.php
@@ -2,7 +2,11 @@
 	require_once('functions.php');
 	$id = $_GET['id'];
 
-	if (isset($id)) view($id);
+	if (isset($id)) {
+		view($id);
+		state_candidates($id);
+		city_candidates($id);
+	}
 ?>
 
 <?php include(HEADER_TEMPLATE); ?>
@@ -11,6 +15,45 @@
 
 	<h2><?php echo $political_party['initials'] . " - " . $political_party['name']; ?></h2>
 	<p><?php echo $political_party['description']; ?></p>
+
+	<h2>Candidatos estaduais</h2>
+	<?php if ($state_candidates) : ?>
+
+		<?php foreach ($state_candidates as $candidate) : ?>
+			
+			<div class="container data-item">
+				<h3><?php echo $candidate['name']; ?></h3>
+				<div>
+					<a class="btn btn-primary" href="../state-candidates/view.php?id=<?php echo $candidate['id']; ?>">Visualizar</a>
+				</div>
+			</div>
+		<?php endforeach; ?>
+
+	<?php else : ?>
+
+		<p>Nenhum candidato estadual cadastrado para o partido.</p>
+
+	<?php endif; ?>
+
+	<br><br>
+	<h2>Candidatos municipais</h2>
+	<?php if ($city_candidates) : ?>
+
+		<?php foreach ($city_candidates as $candidate) : ?>
+			
+			<div class="container data-item">
+				<h3><?php echo $candidate['name']; ?></h3>
+				<div>
+					<a class="btn btn-primary" href="../city-candidates/view.php?id=<?php echo $candidate['id']; ?>">Visualizar</a>
+				</div>
+			</div>
+		<?php endforeach; ?>
+
+	<?php else : ?>
+
+		<p>Nenhum candidato municipal cadastrado para o partido.</p>
+
+	<?php endif; ?>
 
 <?php endif; ?>
 
